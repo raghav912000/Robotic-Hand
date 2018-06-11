c45d3cfd8c0bdc151b1e7a60a64cd0d0fc1087a7
raghav912000 <34455106+raghav912000@users.noreply.github.com>
2018-06-11 18:30:06 +0530

    Add files via upload
    To see my documentation Video click the following link :- 
    https://drive.google.com/file/d/1-rrekSYKpN_owFqER1z6JaDDRBo05Ojp/view?usp=sharing

---
+++ sketch_oct28a.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <nRF24L01.h>
+#include<Servo.h>
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  myservo1.attach(3);
+  myservo2.attach(5);
+  myservo3.attach(6);
+  myservo4.attach(9);
+}
+void loop()
+{
+  int val1=analogRead(A1);
+  int pos1=map(val1,730,775,180,0);
+  myservo1.write(pos1);
+  delay(1);
+  int val2=analogRead(A2);
+  int pos2=map(val2,830,770,0,180);
+  myservo2.write(pos2);
+  delay(1);
+  int val3=analogRead(A3);
+  int pos3=map(val3,790,710,0,180);
+  myservo3.write(pos3);
+  delay(1);
+  int val4=analogRead(A4);
+  int pos4=map(val4,935,975,180,0);
+  myservo4.write(pos4);
+  delay(1);  
+}
+
